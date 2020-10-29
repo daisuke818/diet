a992e44f538dc78edd0a1dc4a3ae0918e5fd51ab
daisuke818 <puyo.puyo456@icloud.com>
2020-10-29 20:11:29 +0900

    first commit

---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+  <title>調味料管理ツール</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
